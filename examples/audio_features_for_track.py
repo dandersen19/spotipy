fa6bbe0519d6b22577a9d2628ab97ca8b0b59fd7
Drake Andersen <53623035+dandersen19@users.noreply.github.com>
2021-01-20 22:05:53 -0500

    Update audio_features_for_track.py

--- examples/audio_features_for_track.py
+++ examples/audio_features_for_track.py
@@ -7,7 +7,6 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import json
 import spotipy
 import time
-import sys
 import os
 
 os.environ['SPOTIPY_CLIENT_ID'] = '5a9702c739a346de80cb333b6cb65e49'
@@ -17,12 +16,8 @@ client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 sp.trace = True
 
-if len(sys.argv) > 1:
-    tids = sys.argv[1:]
-    print(tids)
-
-    start = time.time()
-    features = sp.audio_features(tids)
-    delta = time.time() - start
-    print(json.dumps(features, indent=4))
-    print("features retrieved in %.2f seconds" % (delta,))
+start = time.time()
+features = sp.audio_features('6Xk5AaN4n4SnW71473GI7A')
+delta = time.time() - start
+print(json.dumps(features, indent=4))
+print("features retrieved in %.2f seconds" % (delta,))
