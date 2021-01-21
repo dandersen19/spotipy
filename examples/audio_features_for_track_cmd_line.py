48c138351d45482ddbf211d490489fe4ad92b570
Drake Andersen <53623035+dandersen19@users.noreply.github.com>
2021-01-20 22:20:18 -0500

    Create audio_features_for_track_cmd_line.py

---
+++ examples/audio_features_for_track_cmd_line.py
@@ -0,0 +1,26 @@
+# shows acoustic features for tracks for the given artist
+
+from __future__ import print_function    # (at top of module)
+from spotipy.oauth2 import SpotifyClientCredentials
+import json
+import spotipy
+import time
+import sys
+import os
+
+os.environ['SPOTIPY_CLIENT_ID'] = '5a9702c739a346de80cb333b6cb65e49'
+os.environ['SPOTIPY_CLIENT_SECRET'] = '71c46401434c4a2bbc7d7ae1f03b7797'
+
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+sp.trace = True
+
+if len(sys.argv) > 1:
+    tids = sys.argv[1:]
+    print(tids)
+
+    start = time.time()
+    features = sp.audio_features(tids)
+    delta = time.time() - start
+    print(json.dumps(features, indent=4))
+    print("features retrieved in %.2f seconds" % (delta,))
